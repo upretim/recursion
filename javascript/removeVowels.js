2ef99650028b5800533184c9e635b1af2e28e780
upretim <manojupreti83@yahoo.co.in>
2021-07-11 22:04:45 +0530

    added code

--- javascript/removeVowels.js
+++ javascript/removeVowels.js
@@ -1,14 +1,17 @@
-function removeVowels(){
-    let str= "hello world";
-    console.log(removeVowels_helper(str,0, ""));
 
+/* This function removes all the vowels from a string using recursion
+ for detail explanation please watch
+ https://youtu.be/QRW43YPDcNM
+*/
+function removeVowels(str){
+    let str = "hello world";
+    return removeVowels_helper(str, 0, "");
 }
 
- function removeVowels_helper(str, length, result){
-    if(str.length==length) return result;
-    let vowels = "aeiou";
-    if(vowels.includes(str.charAt(length)))
-        return removeVowels_helper(str,length+1, result);
-    return removeVowels_helper(str,length+1, result + str.charAt(length));
+function removeVowels_helper(str, length, result){
+  if(str.length==length) return result;
+  let vowels = "aeiou";
+   if(vowels.includes(str.charAt(length))) 
+      return removeVowels_helper(str, length+1, result);
+   return removeVowels_helper(str, length+1, result+ str.charAt(length));
 }
-removeVowels();
