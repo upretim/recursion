a76472771194fad1b17e7a2842dcd34db534f218
upretim <manojupreti83@yahoo.co.in>
2021-07-14 23:31:19 +0530

    added print number problem

--- java/src/PowerCalculator.java
+++ java/src/PowerCalculator.java
@@ -1,16 +1,16 @@
 public class PowerCalculator {
     public static void main(String[] args) {
-        System.out.println(getPower(3,2));
-        System.out.println(getPowerII(3,5));
+        System.out.println(calculatePower(3,2));
+        System.out.println(calculatePowerII(3,5));
     }
     /*these 2 functions calculates Math.pow(a,b) using recursion*/
-    private static int getPower(int a, int b){
+    private static int calculatePower(int a, int b){
         if(b==0) return 1;
-        return a*getPower(a,b-1);
+        return a*calculatePower(a,b-1);
     }
-    private static int getPowerII(int a, int b){
+    private static int calculatePowerII(int a, int b){
         if(b==0) return 1;
-        if(b%2==0)  return getPowerII(a,b/2)*getPowerII(a,b/2);
-        return a*getPowerII(a,b/2)*getPowerII(a,b/2);
+        if(b%2==0)  return calculatePowerII(a,b/2)*calculatePowerII(a,b/2);
+        return a*calculatePowerII(a,b/2)*calculatePowerII(a,b/2);
     }
 }
--- javascript/powerCalculator.js
+++ javascript/powerCalculator.js
@@ -1,13 +1,13 @@
  /*these 2 functions calculates Math.pow(a,b) using recursion*/
-    function getPower(a, b){
+    function calculatePower(a, b){
         if(b==0) return 1;
-        return a*getPower(a,b-1);
+        return a*calculatePower(a,b-1);
     }
-    function getPowerII(a, b){
+    function calculatePowerII(a, b){
         if(b==0) return 1;
-        if(b%2==0) return getPowerII(a ,Math.floor(b/2))*getPowerII( a, Math.floor(b/2) );
-        return a * getPowerII(a, Math.floor(b/2)) * getPowerII(a, Math.floor(b/2));
+        if(b%2==0) return calculatePowerII(a ,Math.floor(b/2))*calculatePowerII( a, Math.floor(b/2) );
+        return a * calculatePowerII(a, Math.floor(b/2)) * calculatePowerII(a, Math.floor(b/2));
     }
 
-    console.log(getPower(3,2));
-    console.log(getPowerII(3,5));
+    console.log(calculatePower(3,2));
+    console.log(calculatePowerII(3,5));
